fac4a2f13051615dcefafd7100c49f1472ff62fd
An Le <giaosuan@gmail.com>
2022-01-27 16:09:15 -0800

    updates

--- data-import.R
+++ data-import.R
@@ -17,3 +17,22 @@ file.copy(fullpath,getwd())
 
 #check if file exists
 file.exists(filename)
+
+
+library(readr)
+url <- "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data"
+dat <- read_csv(url,col_names = FALSE)
+
+nrow(dat)
+dim(dat)
+
+#connect to SQL server tables
+library(odbc)
+library(RODBC)
+library(sqldf)
+library(htmltools)
+conn <- odbcDriverConnect('driver={SQL Server};server=DUCK;database=DW_Staging;trusted_connection=true')
+
+DataSQL <- sqlQuery(conn,"SELECT top 5 *  FROM DW_Staging.acu.CDPS_tmp_inelig;"); 
+
+DataSQL
